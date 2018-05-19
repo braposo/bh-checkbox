2ce5745045036cfc52f0ce97710b7f77ab282f4f
Bernardo Raposo <braposo@gmail.com>
2018-05-19 18:14:09 +0100

    Adds CheckboxController
    This component will hold all the logic and state and let the visuals be determined by its children

---
+++ src/CheckboxController.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class CheckboxController extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { checked: this.isChecked({ checked: props.defaultChecked }) };
+    }
+
+    // Check if checkbox is checked based on whether it's a controlled
+    // component or not. If it is gets value from props, if not gets
+    // value from internal state
+    isChecked(state = this.state, props = this.props) {
+        return this.isControlled() ? props.checked : state.checked;
+    }
+
+    // Check if checked prop is defined, meaning it's a controlled component
+    isControlled() {
+        return this.props.checked !== undefined;
+    }
+
+    // Check if it's disabled
+    isDisabled() {
+        return this.props.disabled === true;
+    }
+
+    //
+    toggleCheckbox = () => {
+        // Change internal state if isn't disabled
+        if (!this.isDisabled()) {
+            const newState = !this.isChecked();
+            this.setState({ checked: newState }, this.props.onChange(newState));
+        }
+    };
+
+    // Using render props pattern to send current checked state,
+    // disable prop and toggle function to be called by children
+    // to change the state
+    render() {
+        return this.props.children({
+            checked: this.isChecked(),
+            disabled: this.isDisabled(),
+            toggle: this.toggleCheckbox,
+        });
+    }
+}
+
+CheckboxController.propTypes = {
+    defaultChecked: PropTypes.bool,
+    checked: PropTypes.bool,
+    children: PropTypes.oneOfType([PropTypes.func, PropTypes.array]).isRequired,
+};
+
+CheckboxController.defaultProps = {
+    defaultChecked: false,
+    onChange: () => {}, //need this noop to avoid error in toggleCheckbox
+};
+
+export default CheckboxController;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
+import CheckboxController from "./CheckboxController";
 
-const App = () => <div>start</div>;
+const Checkbox = props => (
+    <CheckboxController {...props}>
+        {({ checked, disabled, toggle }) => <div onClick={toggle}>{checked ? "yes" : "no"}</div>}
+    </CheckboxController>
+);
+
+const App = () => <Checkbox />;
 
 render(<App />, document.getElementById("root"));
