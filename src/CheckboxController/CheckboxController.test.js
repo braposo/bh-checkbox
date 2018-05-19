22a45e8a3b28a318dfd79df4e061fbf9491e8de2
Bernardo Raposo <braposo@gmail.com>
2018-05-19 22:44:22 +0100

    Enables tests by default
    That means that the controlled native checkbox won't work but I think it's better to have the tests running instead

--- src/CheckboxController/CheckboxController.test.js
+++ src/CheckboxController/CheckboxController.test.js
@@ -2,52 +2,52 @@
 // input when using this test. Not sure why and couldn't spend the time
 // debugging to be honest. You can uncomment the file and run the tests.
 
-// import React from "react";
-// import { render, Simulate } from "react-testing-library";
-// import CheckboxController from "./CheckboxController";
+import React from "react";
+import { render, Simulate } from "react-testing-library";
+import CheckboxController from "./CheckboxController";
 
-// describe("CheckboxController", () => {
-//     it("renders child components", () => {
-//         const Checkbox = props => (
-//             <CheckboxController {...props} data-testid="checkbox">
-//                 {({ checked, disabled, toggle }) => <div data-testid="checkbox-child">Hello</div>}
-//             </CheckboxController>
-//         );
+describe("CheckboxController", () => {
+    it("renders child components", () => {
+        const Checkbox = props => (
+            <CheckboxController {...props} data-testid="checkbox">
+                {({ checked, disabled, toggle }) => <div data-testid="checkbox-child">Hello</div>}
+            </CheckboxController>
+        );
 
-//         const { getByTestId } = render(<Checkbox />);
-//         expect(getByTestId("checkbox-child")).not.toBeNull();
-//     });
+        const { getByTestId } = render(<Checkbox />);
+        expect(getByTestId("checkbox-child")).not.toBeNull();
+    });
 
-//     it("sends checked value to child component", () => {
-//         const Checkbox = props => (
-//             <CheckboxController {...props} data-testid="checkbox">
-//                 {({ checked, disabled, toggle }) => (
-//                     <div data-testid="checkbox-child" checked={checked}>
-//                         Hello
-//                     </div>
-//                 )}
-//             </CheckboxController>
-//         );
+    it("sends checked value to child component", () => {
+        const Checkbox = props => (
+            <CheckboxController {...props} data-testid="checkbox">
+                {({ checked, disabled, toggle }) => (
+                    <div data-testid="checkbox-child" checked={checked}>
+                        Hello
+                    </div>
+                )}
+            </CheckboxController>
+        );
 
-//         const { getByTestId } = render(<Checkbox />);
-//         expect(getByTestId("checkbox-child")).toHaveProperty("checked");
-//     });
+        const { getByTestId } = render(<Checkbox />);
+        expect(getByTestId("checkbox-child")).toHaveProperty("checked");
+    });
 
-//     it("updates checked value with toggle function", () => {
-//         const Checkbox = props => (
-//             <CheckboxController {...props} data-testid="checkbox">
-//                 {({ checked, disabled, toggle }) => (
-//                     <div data-testid="checkbox-child" checked={checked} onClick={toggle}>
-//                         Hello
-//                     </div>
-//                 )}
-//             </CheckboxController>
-//         );
+    it("updates checked value with toggle function", () => {
+        const Checkbox = props => (
+            <CheckboxController {...props} data-testid="checkbox">
+                {({ checked, disabled, toggle }) => (
+                    <div data-testid="checkbox-child" checked={checked} onClick={toggle}>
+                        Hello
+                    </div>
+                )}
+            </CheckboxController>
+        );
 
-//         const { getByTestId } = render(<Checkbox />);
-//         const childComponent = getByTestId("checkbox-child");
-//         expect(childComponent).toHaveProperty("checked", false);
-//         Simulate.click(childComponent);
-//         expect(childComponent).toHaveProperty("checked", true);
-//     });
-// });
+        const { getByTestId } = render(<Checkbox />);
+        const childComponent = getByTestId("checkbox-child");
+        expect(childComponent).toHaveProperty("checked", false);
+        Simulate.click(childComponent);
+        expect(childComponent).toHaveProperty("checked", true);
+    });
+});
